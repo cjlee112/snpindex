9c6759a31005412be5a64a46fa3d0f710dcb4153
Christopher Lee <leec@chem.ucla.edu>
2012-03-16 18:22:13 -0700

    switched to standard package structure and setuptools

--- setup.py
+++ setup.py
@@ -1,25 +1,33 @@
-from distutils.core import setup, Extension
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    print 'Setuptools not imported, falling back to distutils'
+    from distutils.core import setup, Extension
+
 import os,sys
 
-module1 = Extension('snpindex',
-                    define_macros = [('MAJOR_VERSION', '1'),
-                                     ('MINOR_VERSION', '0'),
+if 'setuptools' in sys.modules:
+    cmdclass = {}
+else:
+    from Pyrex.Distutils import build_ext
+    cmdclass = {'build_ext': build_ext}
+
+module1 = Extension('snpindex.core',
+                    define_macros = [('MAJOR_VERSION', '0'),
+                                     ('MINOR_VERSION', '1'),
                                      ('USE_PROJECT_HEADER', '1')],
-                    sources = ['snpindex.c', 'cgraph.c'])
+                    sources = [os.path.join('snpindex', 'core.pyx'),
+                               os.path.join('snpindex', 'cgraph.c')])
 
 
-if os.access('snpindex.c',os.R_OK):
-    print 'Using existing pyrexc-generated C-code...'
-else:  # HMM, NO PYREXC COMPILED CODE, HAVE TO RUN PYREXC
-    exit_status=os.system('pyrexc snpindex.pyx') # TRY USING PYREX TO COMPILE EXTENSIONS
-    if exit_status!=0:  # CAN'T RUN THE PYREX COMPILER TO PRODUCE C
-        print '\n\nPyrex compilation failed!  Is pyrex missing or not in your PATH?'
-        sys.exit(1) # EXIT WITH ERROR CODE
 
 setup (name = 'snpindex',
        version = '0.1',
-       long_description = '''
-Common Ancestor Profile functionality as python module.
-''',
-       ext_modules = [module1])
+       long_description = '''Fast SNP indexing and pair analysis.''',
+       author = "Christopher Lee",
+       author_email='leec@chem.ucla.edu',
+       packages = ['snpindex'],
+       ext_modules = [module1],
+       cmdclass = cmdclass,
+       )
 
---
+++ snpindex/__init__.py
@@ -0,0 +1,3 @@
+from core import SNPDataset, PairAnalysis, SNPSamples, IntSet
+
+__all__ = ('SNPDataset', 'PairAnalysis', 'SNPSamples', 'IntSet')
